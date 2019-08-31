7f46403b1a937c53aa682aca0ae9f562167ad9fe
Jamil Spain <jamil.spain@us.ibm.com>
2019-08-31 17:47:39 -0400

    feat(tutorial): complete step 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './app.scss';
+//import { Button } from 'carbon-components-react';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
+
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
